b5ee489caec5508da5376b0ba1a8d032da871cb7
Bruemor <arymer1@gmail.com>
2016-12-26 10:18:11 -0500

    went through and set up the navigation for each project page. Now I just need to bring in all the writeup and begin styling

---
+++ app/projects/plaza_at_university/plaza.js
@@ -0,0 +1,12 @@
+
+
+
+(function(){
+
+  var summaryToggle = document.getElementById('summary').addEventListener('click', function(event){
+
+    document.getElementById('summary').style.display = ' ';
+
+  })
+
+}());

ea125f69535de453f1327f514400fa0a5e67a7e4
Bruemor <arymer1@gmail.com>
2016-12-30 16:41:20 -0500

    finalized write up and styling as well as summary toggle functionality

---
+++ app/projects/waterleigh/waterleigh.js
@@ -0,0 +1,12 @@
+
+
+
+(function(){
+
+  var summaryToggle = document.getElementById('summary').addEventListener('click', function(event){
+
+    $('#waterleigh-summary').toggle('slow');
+
+  })
+
+}());

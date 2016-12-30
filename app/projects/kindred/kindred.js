ca9a44522f77628154f77ad1b6b539c26b1c95f5
Bruemor <arymer1@gmail.com>
2016-12-30 16:38:34 -0500

    finalized styling and writeup. Navigation works as well as summary toggle functionality. Still need to work on background image navigation

---
+++ app/projects/kindred/kindred.js
@@ -0,0 +1,13 @@
+
+
+
+
+(function(){
+
+  var summaryToggle = document.getElementById('summary').addEventListener('click', function(event){
+
+    $('#kindred-summary').toggle('slow');
+
+  })
+
+}());

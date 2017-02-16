9f306d19e51538ca3977a8c002755a9ebeb8ba1f
Bruemor <arymer1@gmail.com>
2017-02-16 16:21:06 -0500

    added background functionality

--- app/projects/sea_isle/sea-isle.js
+++ app/projects/sea_isle/sea-isle.js
@@ -11,6 +11,31 @@
 
     $('#sea-isle-summary').toggle('slow');
 
-  })
+  });
+
+
+  var imageOneScroll = document.getElementById('scroll-1').addEventListener('click', function(event){
+
+    window.scrollTo(0, 1080);
+
+  });
+
+  var imageTwoScroll = document.getElementById('scroll-2').addEventListener('click', function(event){
+
+    window.scrollTo(0, 2160);
+
+  });
+
+  var imageThreeScroll = document.getElementById('scroll-3').addEventListener('click', function(event){
+
+    window.scrollTo(0, 3240);
+
+  });
+
+  var imageFourScroll = document.getElementById('scroll-4').addEventListener('click', function(event){
+
+    window.scrollTo(0, 0);
+
+  });
 
 }());

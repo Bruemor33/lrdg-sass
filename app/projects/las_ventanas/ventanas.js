e1d7bcf8e7dd5dc35fc75e372a8979060b44422c
Bruemor <arymer1@gmail.com>
2017-04-23 19:05:07 -0400

    Added new smooth scroll functionality.

--- app/projects/las_ventanas/ventanas.js
+++ app/projects/las_ventanas/ventanas.js
@@ -10,28 +10,12 @@
 
   });
 
-  var imageOneScroll = document.getElementById('scroll-1').addEventListener('click', function(event){
+  $('#scroll ul li a[href^="#"]').click(function(event){
+    event.preventDefault();
 
-    window.scrollTo(0, 1080);
-
-  })
-
-  var imageTwoScroll = document.getElementById('scroll-2').addEventListener('click', function(event){
-
-    window.scrollTo(0, 2160);
-
-  });
-
-  var imageThreeScroll = document.getElementById('scroll-3').addEventListener('click', function(event){
-
-    window.scrollTo(0, 3240);
-
-  });
-
-  var imageFourScroll = document.getElementById('scroll-4').addEventListener('click', function(event){
-
-    window.scrollTo(0, 0);
-
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 500);
   });
 
 }());
